064c76f963e1f0bca0a2265d4705a9e88edb926e
Claire <claire.rouss@gmail.com>
2023-02-16 16:44:31 +0100

    axios + auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,33 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'SR1wKQYqlTLVWZSlYkot3xTu0qdZuWDn');
-        const compteId = decodedToken.compteID;
-        req.auth = {
-            compteId: compteId
+
+        let token;
+        if (req.headers.authorization) {
+            token = req.headers.authorization.split(' ')[1];
+        } else if (req.body.username && req.body.password) {
+            const {data} = await axios.post("https://backend-tp-final-nodejs.agence-pixi.fr/wow/compte/check", {
+                    username: req.body.username,
+                    password: req.body.password
+                }
+            )
+            console.log(data)
+            token = data.token;
+            // TODO : renvoyer le token à l'utilisateur
+        }
+        if (token) {
+            const decodedToken = jwt.verify(token, 'SR1wKQYqlTLVWZSlYkot3xTu0qdZuWDn');
+            compteId = decodedToken.compteID;
+            req.auth = {
+                compteId: compteId
+            }
+        } else {
+            res.status(401).json({message: "Authentification manquante"})
         }
         next();
-    } catch(error) {
+    } catch (error) {
         res.status(401).json({error})
     }
 }
